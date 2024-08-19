1d9c44d7460463d8b28ae186565662b65663f94b
Billy <williamrobertsjr@gmail.com>
2024-08-18 22:27:33 -0400

    first commit

---
+++ js/swipers.js
@@ -0,0 +1,8 @@
+const swiper = new Swiper('.swiper', {
+    speed: 400,
+    spaceBetween: 100,
+    autoplay: {
+        delay: 2000,
+      },
+      loop: true,
+  });
---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js,twig,php}","./views/*.{twig,html}"],
+  theme: {
+    extend: {
+      colors: {
+        // Extend with your colors
+        'black': '#222222',
+        'red': '#EE3823',
+        'gold': '#F5AF00',
+        'green': '#047658',
+        'blue': '#024DA1',
+        'tan': '#FFE5C0',
+        'white': '#f7f7f7'
+      },
+      borderWidth: {
+        '3': '3px',
+        '6': '6px',
+      },
+      borderColor: theme => ({
+        ...theme('colors'),
+       
+      }),
+      backgroundColor: theme => ({
+        ...theme('colors'),
+       
+      }),
+    },
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+}
+
